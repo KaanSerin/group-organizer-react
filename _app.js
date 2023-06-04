526ed120d3fcb85bcc1013428b720fa590f194cf
KaanSerin <kaanserin113@gmail.com>
2023-06-04 14:15:16 +0300

    Remove unused component

--- _app.js
+++
@@ -1,7 +0,0 @@
-// add bootstrap css
-import 'react-bootstrap/dist/react-bootstrap.min.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
