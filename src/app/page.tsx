d9dee608cabaa0e77c0c58cfd10b9b160b95e455
KaanSerin <kaanserin113@gmail.com>
2023-06-07 17:19:36 +0300

    Forward user on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,18 @@
 'use client';
-
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import axios from '@/libs/axios';
-import { Button } from 'react-bootstrap';
 
 export default function Home() {
-  const errorMethod = async () => {
-    try {
-      await axios.get('auth/profile');
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const onLogout = async () => {
-    try {
-      await axios.post('auth/logout');
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  const router = useRouter();
 
   useEffect(() => {
-    errorMethod();
-  }, []);
+    const auth = localStorage.getItem('user');
+    if (auth) {
+      router.push('/dashboard');
+    } else {
+      router.push('/signin');
+    }
+  }, [router]);
 
-  return (
-    <main>
-      <div>Home</div>
-      <Button onClick={onLogout}>Logout</Button>
-    </main>
-  );
+  return <></>;
 }
