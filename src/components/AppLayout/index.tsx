78ecd62514e38eb969851d77213319b6c0e46468
KaanSerin <kaanserin113@gmail.com>
2023-06-07 20:57:15 +0300

    Forward to sign in on no auth

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -1,7 +1,17 @@
-import { ReactNode } from 'react';
+'use client';
+import { ReactNode, useEffect } from 'react';
 import Sidebar from '@/components/Sidebar';
+import { useRouter } from 'next/navigation';
 
 export default function AppLayout({ children }: { children: ReactNode }) {
+  const router = useRouter();
+  useEffect(() => {
+    const auth = localStorage.getItem('user');
+    if (!auth) {
+      router.push('/signin');
+    }
+  }, [router]);
+
   return (
     <main className="d-flex">
       <Sidebar />
