beb67a7a9b03836a54187ddc76755819228dc93d
KaanSerin <kaanserin113@gmail.com>
2023-06-08 21:58:37 +0300

    Implement Groups page component

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,7 +1,42 @@
+'use client';
+import { Button } from 'react-bootstrap';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './index.module.scss';
+import { useEffect, useState } from 'react';
+import axios from '@/libs/axios';
+import GroupCard, { GroupInfo } from '@/components/GroupCard';
+
 export default function Groups() {
+  const [groups, setGroups] = useState<GroupInfo[]>([]);
+
+  const getUserGroups = async () => {
+    try {
+      const res = await axios.get('groups/user_groups');
+      setGroups(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getUserGroups();
+  }, []);
+
   return (
     <div>
-      <div>Groups</div>
+      <section className={'groups-header d-flex'}>
+        <h3 className={'fw-semibold'}>Groups</h3>
+        <Button className={styles.createGroupBtn} variant={'outline-purple'}>
+          <FontAwesomeIcon icon={faPlus} className={'me-2'} />
+          <span>Create Group</span>
+        </Button>
+      </section>
+      <section className={styles.groupsContainer}>
+        {groups.map((group: GroupInfo) => (
+          <GroupCard key={group.id} info={group} />
+        ))}
+      </section>
     </div>
   );
 }
