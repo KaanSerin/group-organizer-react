106c57450db549f719e9bd5da451e286b2c52988
KaanSerin <kaanserin113@gmail.com>
2023-06-09 19:37:04 +0300

    Implement custom data fetcher function

---
+++ src/libs/fetch.ts
@@ -0,0 +1,13 @@
+import { cookies } from 'next/headers';
+
+export default async function fetchUrl(url: string) {
+  const cookieStore = cookies();
+  const headers = new Headers();
+  headers.append('Cookie', cookieStore);
+  const postFix = url.charAt(0) === '/' ? url.slice(1) : url;
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${postFix}`, {
+    credentials: 'include',
+    headers
+  });
+  return res.json();
+}
