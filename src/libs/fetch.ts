f1cf4ac54d9565c48c355d03faaac401be3f70af
KaanSerin <kaanserin113@gmail.com>
2023-06-11 16:09:52 +0300

    Fix type error

--- src/libs/fetch.ts
+++ src/libs/fetch.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers';
 export default async function fetchUrl(url: string) {
   const cookieStore = cookies();
   const headers = new Headers();
-  headers.append('Cookie', cookieStore);
+  headers.append('Cookie', cookieStore.toString());
   const postFix = url.charAt(0) === '/' ? url.slice(1) : url;
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${postFix}`, {
     credentials: 'include',
