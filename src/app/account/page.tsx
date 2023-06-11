317b24d68c45bc199472575d0579064859a9079a
KaanSerin <kaanserin113@gmail.com>
2023-06-11 13:54:03 +0300

    Create accounts page component

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,7 +1,23 @@
-export default function Account() {
+import styles from './index.module.scss';
+import ProfileCard from '@/components/ProfileCard';
+import fetchUrl from '@/libs/fetch';
+
+const getUserProfile = async () => {
+  try {
+    return await fetchUrl('/auth/profile');
+  } catch (e) {
+    console.log(e);
+    return {};
+  }
+};
+
+export default async function Account() {
+  const profile = await getUserProfile();
+
   return (
     <div>
-      <div>Account</div>
+      <h3 className={styles.pageTitle}>Account</h3>
+      <ProfileCard profile={profile} />
     </div>
   );
 }
