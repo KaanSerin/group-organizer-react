30cb6775bccf8e18f7f9f845cb60d60e1b63ae02
KaanSerin <kaanserin113@gmail.com>
2023-07-05 11:57:30 +0300

    Update table params

--- src/components/GroupMembers/index.tsx
+++ src/components/GroupMembers/index.tsx
@@ -1,7 +1,27 @@
 import CustomTable, { ActionButton } from '@/components/CustomTable';
+import { UserProfile } from '@/components/ProfileCard';
+import { useParams } from 'next/navigation';
 
 export default function GroupMembers() {
-  const columns = ['ID', 'Email', 'Name'];
+  const params = useParams();
+  const columns = [
+    {
+      label: 'ID',
+      renderCell: (item: UserProfile) => item.id
+    },
+    {
+      label: 'Username',
+      renderCell: (item: UserProfile) => item.username
+    },
+    {
+      label: 'First Name',
+      renderCell: (item: UserProfile) => item.firstName
+    },
+    {
+      label: 'Last Name',
+      renderCell: (item: UserProfile) => item.lastName
+    }
+  ];
 
   const buttons: ActionButton[] = [
     {
@@ -14,11 +34,12 @@ export default function GroupMembers() {
   return (
     <div>
       <CustomTable
-        fetchUrl={'/groups/1/members'}
+        fetchUrl={`/groups/${params.id}/members`}
         columns={columns}
         actions={true}
         buttons={buttons}
         title={'Members'}
+        pageLength={15}
       />
     </div>
   );
