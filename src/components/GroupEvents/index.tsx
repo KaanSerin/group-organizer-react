c1aa5d94c39cf1c7c06d8ed0a641fa081f594d0f
KaanSerin <kaanserin113@gmail.com>
2023-07-22 19:56:30 +0300

    Pass fetch url as param

--- src/components/GroupEvents/index.tsx
+++ src/components/GroupEvents/index.tsx
@@ -1,10 +1,13 @@
 import styles from './index.module.scss';
 import EventsListScroller from '@/components/EventsListScroller';
+import { useParams } from 'next/navigation';
 
 export default function GroupEvents() {
+  const params = useParams();
+
   return (
     <div className={styles.groupEvents}>
-      <EventsListScroller />
+      <EventsListScroller fetchUrl={`/groups/${params.id}/events`} />
     </div>
   );
 }
