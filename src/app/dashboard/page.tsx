b9ab67315e0b7a179b65ef82cd15f60d5628f736
KaanSerin <kaanserin113@gmail.com>
2023-07-22 19:56:46 +0300

    Implement upcoming events page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,11 @@
+import EventsListScroller from '@/components/EventsListScroller';
+
 export default function Dashboard() {
   return (
-    <div>
-      <div>Dashboard</div>
+    <div className={'h-100 pb-2'}>
+      <h3 className={'fw-semibold'}>Upcoming Events</h3>
+
+      <EventsListScroller fetchUrl={`/groups/upcoming_events`} />
     </div>
   );
 }
